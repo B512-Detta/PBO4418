3d408b7d9707ec54ba8d7eb833d616e03ab1c7b2
B512-Detta <80021380+B512-Detta@users.noreply.github.com>
2021-03-15 15:31:12 -1200

    Add files via upload

---
+++ Pertemuan 1/Praktik1/src/praktik1/ReadConsole.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktik1;
+
+/**
+ *
+ * @author acer
+ */
+import java.io.*;
+public class ReadConsole {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        InputStreamReader cin = null;
+        
+        try{
+            cin = new InputStreamReader(System.in);
+            System.out.println("Enter characters, 'q' to quit.");
+            char c;
+            do {
+                c = (char) cin.read();
+                System.out.print(c);
+            } while (c != 'q');
+        } finally {
+            if(cin != null){
+                cin.close();
+            }
+        }
+    }
+    
+}
---
+++ Pertemuan 1/Praktik2/src/praktik2/TV.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktik2;
+
+/**
+ *
+ * @author acer
+ */
+public class TV extends TestTV {
+   int channel;
+   int volume;
+   boolean on = false;
+   public TV(){
+   }
+   public void TurnOn(){
+       on = true;
+   }
+   public void setchannel(int newchannel) {
+       channel = newchannel;
+   }
+   public void setvolume(int newvolume){
+       volume = newvolume;
+   }
+   public void channelUp(){
+       channel = channel+1;
+   }
+   public void channelDown(){
+       channel = channel-1;
+   }
+   public void volumeUp(){
+       volume = volume+1;
+   }
+   public void volumeDown(){
+       volume = volume-1;
+   }
+}
+
